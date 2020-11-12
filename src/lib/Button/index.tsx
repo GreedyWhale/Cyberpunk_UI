9aa013e440781d7441190437c98f41272cd20971
Caisr <383911973@qq.com>
2020-11-12 17:45:14 +0800

    feat: 增加button组件text类型

--- src/lib/Button/index.tsx
+++ src/lib/Button/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import style from './style.module.scss'
 import { ButtonProps } from '@/types/button'
 import { useTheme } from './hooks'
 
 const Button: React.FunctionComponent<ButtonProps> = (props) => {
   const [theme] = useTheme(props)
+  const [visible, setVisible] = useState(false)
+  useEffect(() => { setVisible(true) }, [])
 
   return (
     <button
+      data-visible={visible}
       data-theme={theme}
       data-type={props.type}
       data-disabled={props.disable}
